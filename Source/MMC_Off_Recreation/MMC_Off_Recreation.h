f89a865a0216c2e0c508949af63346a7db2d1a43
MMC-Scholars <latrunkster@gmail.com>
2018-04-09 17:05:24 -0400

    Added base source files for project - Michael

---
+++ Source/MMC_Off_Recreation/MMC_Off_Recreation.h
@@ -0,0 +1,11 @@
+// This software is under partial ownership by The Ohio State University, 
+//for it is a product of student employees. For official policy, see
+//https://tco.osu.edu/wp-content/uploads/2013/09/PatentCopyrightPolicy.pdf 
+//or contact The Ohio State University's Office of Legal Affairs
+
+#ifndef MMC_Off_Recreation_H
+#define MMC_Off_Recreation_H
+
+#include "CoreMinimal.h"
+
+#endif //MMC_Off_Recreation_H
