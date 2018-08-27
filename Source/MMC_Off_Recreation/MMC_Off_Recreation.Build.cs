72e83eaa5295d0f4e61a304f62a74ae3d548f940
MMC-Scholars <latrunkster@gmail.com>
2018-08-27 12:57:00 -0400

    Added an additional public dependency - Michael

--- Source/MMC_Off_Recreation/MMC_Off_Recreation.Build.cs
+++ Source/MMC_Off_Recreation/MMC_Off_Recreation.Build.cs
@@ -10,16 +10,17 @@ public class MMC_Off_Recreation : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
 
 		PublicIncludePaths.Add("C:/Users/Public/Documents/Public Projects/MMC_Off_Recreation/Source/MMC_Off_Recreation/VRBase");
+        PublicIncludePaths.Add("C:/Users/Public/Public Projects/MMC-Office-Recreation/Source/MMC_Off_Recreation/VRBase");
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
