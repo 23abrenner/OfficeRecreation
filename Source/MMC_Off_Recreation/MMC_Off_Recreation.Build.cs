bf8c24f87a79919f256ee4b3116dcd024cd8c465
MMC-Scholars <latrunkster@gmail.com>
2018-10-25 19:44:19 -0400

    Updated build file documentation - Michael

--- Source/MMC_Off_Recreation/MMC_Off_Recreation.Build.cs
+++ Source/MMC_Off_Recreation/MMC_Off_Recreation.Build.cs
@@ -8,10 +8,12 @@ public class MMC_Off_Recreation : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
+        //These include the libraries need by Unreal Engine and VR Headsets
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore" });
-
 		PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
 
+        //These allow us to reference files in the VRBase directory from anywhere in the project - you may need to add
+        //another path for your machine.
 		PublicIncludePaths.Add("C:/Users/Public/Documents/Public Projects/MMC_Off_Recreation/Source/MMC_Off_Recreation/VRBase");
         PublicIncludePaths.Add("C:/Users/Public/Documents/Public Projects/MMC_Office_Recreation/Source/MMC_Off_Recreation/VRBase");
         PublicIncludePaths.Add("C:/Users/Public/Public Projects/MMC-Office-Recreation/Source/MMC_Off_Recreation/VRBase");
